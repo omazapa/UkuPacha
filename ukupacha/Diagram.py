439664f35011d73b64bf3c921e80b868c13854a6
Omar Zapata <Omar.Zapata@cern.ch>
2022-08-30 14:51:12 -0500

    added required imports, fixed lint problems

--- ukupacha/Diagram.py
+++ ukupacha/Diagram.py
@@ -1,4 +1,6 @@
 from ukupacha.Utils import is_dict
+from blockdiag import parser, builder, drawer
+
 
 def graph2blockdiag(regs, pdb):
     """
@@ -6,36 +8,38 @@ def graph2blockdiag(regs, pdb):
     """
     output = ""
     if is_dict(regs):
-        parent=list(regs.keys())[0]
+        parent = list(regs.keys())[0]
         if regs[parent] == None:
             output = f"'{parent}\n{pdb}';\n"
         else:
             for i in regs[parent]:
-                db=i["DB"]
+                db = i["DB"]
                 sub_regs = i["TABLES"]
-                out = graph2blockdiag(sub_regs,db)
+                out = graph2blockdiag(sub_regs, db)
                 output += f" '{parent}\n{pdb}' -> {out}"
     else:
         for reg in regs:
-            out = graph2blockdiag(reg,pdb)
-            output+=out
+            out = graph2blockdiag(reg, pdb)
+            output += out
     return output
 
-def model2diag(model:dict):
+
+def model2diag(model: dict):
     """
     Given the model dict, takes the grap and the initial db to
     call grap2blockdiag
     """
     graph = model["GRAPH"]
     db = model["CHECKPOINT"]["DB"]
-    out = graph2blockdiag(graph,db)
-    output="diagram { "+out+" }"
+    out = graph2blockdiag(graph, db)
+    output = "diagram { "+out+" }"
     return output
 
-def diag2file(diag,filename,fmt):
+
+def diag2file(diag, filename, fmt):
     """
     Function to save the diag in a file.
-    
+
     Parameters
     ------------
     diag:str
@@ -50,4 +54,3 @@ def diag2file(diag,filename,fmt):
     draw = drawer.DiagramDraw(fmt, diagram, filename=filename)
     draw.draw()
     draw.save()
-
